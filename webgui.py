86b3fd5241984a21bdc0b1418463ab0326fc0bdc
Jani Jansson <jani.jansson@gmail.com>
2017-12-30 16:27:57 -0500

    almost working version of webgui... :)

--- webgui.py
+++ webgui.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+
 # This piece of code creates the web-graphical user interface for showing the
 # data from SQLite3 database.
 # Version: 0.1
@@ -5,7 +8,6 @@
 # Last modified: 30.12.2017
 # Author: Jani Jansson
 
-#!/usr/bin/env python
 
 import sqlite3
 import sys
@@ -14,25 +16,29 @@ import cgitb
 
 # Global variables
 speriod=(15*60)-1
-dbname='/home/jani/Projects/DHT11/temperaturelog.db'
+dbname='/var/www/temperaturelog.db'
 tablename='temperatures'
 
 # Print the HTTP header:
 def printHTTPheader():
-	print "Content-type: text/html\n\n"
+	print "Content-Type: text/html"
+	print
 
 
 # Print the HTML head section.
 # Arguments are the page title and the table for the chart
 def printHTMLhead(title, table):
-	print "<head>"
-	print "	<title>"
+	if title == None:
+		title="New title"
+	else:
+		title=str(title)
+	print "<HEAD>"
+	print "<TITLE>"
 	print title
-	print "	</title>"
-
+	print "</TITLE>"
 	print_graph_script(table)
+	print "</HEAD>"
 
-	print "</head>"
 
 
 # Read data from the database.
@@ -44,7 +50,7 @@ def get_data(interval):
 	if interval == None:
 		curs.execute("SELECT * FROM " % tablename)
 	else:
-		curs.execute("SELECT * FROM " % tablename % " WHERE timestamp>datetime('now','-%s hours')" % interval)
+		curs.execute("SELECT * FROM "+tablename+" WHERE currentdate > date('now','-%s days')" % interval)
 
 	rows=curs.fetchall()
 
@@ -73,188 +79,175 @@ def create_table(rows):
 # pass the table generated from the database info
 def print_graph_script(table):
 
-    # google chart snippet
-    chart_code="""
-    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-    <script type="text/javascript">
-      google.load("visualization", "1", {packages:["corechart"]});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Time', 'Temperature'],
-%s
-        ]);
-        var options = {
-          title: 'Temperature'
-        };
-        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
-    </script>"""
-
-    print chart_code % (table)
+	# google chart snippet
+	chart_code="""
+	<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+	<script type="text/javascript">
+	google.load("visualization", "1", {packages:["corechart"]});
+	google.setOnLoadCallback(drawChart);
+	function drawChart() {
+		var data = google.visualization.arrayToDataTable([['Time', 'Temperature'],%s]);
+		var options = {
+			title: 'Temperature'
+		};
+		var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+		chart.draw(data, options);
+	}
+	</script>"""
+	
+	print chart_code % (table)
 
 
 # Print the div that contains the graph
 
 def show_graph():
-    print "<h2>Temperature Chart</h2>"
-    print '<div id="chart_div" style="width: 900px; height: 500px;"></div>'
+	print "<h2>Temperature Chart</h2>"
+	print '<div id="chart_div" style="width: 900px; height: 500px;"></div>'
 
 
 # connect to the db and show some stats
 # argument option is the number of hours
 def show_stats(option):
-    conn=sqlite3.connect(dbname)
-    curs=conn.cursor()
+	conn=sqlite3.connect(dbname)
+	curs=conn.cursor()
 
-    if option is None:
-        option = str(24)
+	if option is None:
+		option = str(7)
 
-    curs.execute("SELECT timestamp,max(temp) FROM " % tablename % " WHERE timestamp>datetime('now','-%s hour') AND timestamp<=datetime('now')" % option)
-#    curs.execute("SELECT timestamp,max(temp) FROM temps WHERE timestamp>datetime('2017-12-01 00:00:00','-%s hour') AND timestamp<=datetime('2013-09-19 21:31:02')" % option)
-    rowmax=curs.fetchone()
-    rowstrmax="{0}&nbsp&nbsp&nbsp{1}C".format(str(rowmax[0]),str(rowmax[1]))
+	curs.execute("SELECT currentdate,max(temperature),sensor_id FROM "+tablename+" WHERE currentdate > datetime('now','-%s days') AND currentdate <= datetime('now')" % option)
+	# curs.execute("SELECT timestamp,max(temp) FROM temps WHERE timestamp>datetime('2017-12-01 00:00:00','-%s hour') AND timestamp<=datetime('2013-09-19 21:31:02')" % option)
+	rowmax=curs.fetchone()
+	rowstrmax="{0}&nbsp&nbsp&nbsp{1}C".format(str(rowmax[0]),str(rowmax[1]))
 
-    curs.execute("SELECT timestamp,min(temp) FROM " % tablename % " WHERE timestamp>datetime('now','-%s hour') AND timestamp<=datetime('now')" % option)
-#    curs.execute("SELECT timestamp,min(temp) FROM temps WHERE timestamp>datetime('2013-09-19 21:30:02','-%s hour') AND timestamp<=datetime('2013-09-19 21:31:02')" % option)
-    rowmin=curs.fetchone()
-    rowstrmin="{0}&nbsp&nbsp&nbsp{1}C".format(str(rowmin[0]),str(rowmin[1]))
+	curs.execute("SELECT currentdate,min(temperature),sensor_id FROM "+tablename+" WHERE currentdate > datetime('now','-%s days') AND currentdate <= datetime('now')" % option)
+	# curs.execute("SELECT timestamp,min(temp) FROM temps WHERE timestamp>datetime('2013-09-19 21:30:02','-%s hour') AND timestamp<=datetime('2013-09-19 21:31:02')" % option)
+	rowmin=curs.fetchone()
+	rowstrmin="{0}&nbsp&nbsp&nbsp{1}C".format(str(rowmin[0]),str(rowmin[1]))
 
-    curs.execute("SELECT avg(temp) FROM " % tablename % " WHERE timestamp>datetime('now','-%s hour') AND timestamp<=datetime('now')" % option)
-#    curs.execute("SELECT avg(temp) FROM temps WHERE timestamp>datetime('2013-09-19 21:30:02','-%s hour') AND timestamp<=datetime('2013-09-19 21:31:02')" % option)
-    rowavg=curs.fetchone()
+	curs.execute("SELECT currentdate,avg(temperature),sensor_id FROM "+tablename+" WHERE currentdate > datetime('now','-%s days') AND currentdate <= datetime('now')" % option)
+	# curs.execute("SELECT avg(temp) FROM temps WHERE timestamp>datetime('2013-09-19 21:30:02','-%s hour') AND timestamp<=datetime('2013-09-19 21:31:02')" % option)
+	rowavg=curs.fetchone()
+	rowstravg = "{0}C".format(str(rowavg[1]))
 
+	print "<hr>"
 
-    print "<hr>"
+	print "<h2>Minumum temperature&nbsp</h2>"
+	print rowstrmin
+	print "<h2>Maximum temperature</h2>"
+	print rowstrmax
+	print "<h2>Average temperature</h2>"
+	print rowstravg
 
+	print "<hr>"
+	print "<h2>In the last hour:</h2>"
+	print "<table>"
+	print "<tr><td><strong>Date/Time</strong></td><td><strong>Temperature</strong></td></tr>"
 
-    print "<h2>Minumum temperature&nbsp</h2>"
-    print rowstrmin
-    print "<h2>Maximum temperature</h2>"
-    print rowstrmax
-    print "<h2>Average temperature</h2>"
-    print "%.3f" % rowavg+"C"
-
-    print "<hr>"
-
-    print "<h2>In the last hour:</h2>"
-    print "<table>"
-    print "<tr><td><strong>Date/Time</strong></td><td><strong>Temperature</strong></td></tr>"
-
-    rows=curs.execute("SELECT * FROM " % temperature % " WHERE timestamp>datetime('new','-1 hour') AND timestamp<=datetime('new')")
-#    rows=curs.execute("SELECT * FROM temps WHERE timestamp>datetime('2013-09-19 21:30:02','-1 hour') AND timestamp<=datetime('2013-09-19 21:31:02')")
-    for row in rows:
-        rowstr="<tr><td>{0}&emsp;&emsp;</td><td>{1}C</td></tr>".format(str(row[0]),str(row[1]))
-        print rowstr
-    print "</table>"
-
-    print "<hr>"
-
-    conn.close()
+	rows=curs.execute("SELECT * FROM "+tablename+" WHERE currentdate > datetime('new','-1 days') AND currentdate <= datetime('new')")
+	# rows=curs.execute("SELECT * FROM temps WHERE timestamp>datetime('2013-09-19 21:30:02','-1 hour') AND timestamp<=datetime('2013-09-19 21:31:02')")
+	for row in rows:
+		rowstr="<tr><td>{0}&emsp;&emsp;</td><td>{1}C</td></tr>".format(str(row[0]),str(row[1]))
+	print rowstr
+	print "</table>"
+	print "<hr>"
+	
+	conn.close()
 
 
 def print_time_selector(option):
 
-    print """<form action="/cgi-bin/webgui.py" method="POST">
-        Show the temperature logs for  
-        <select name="timeinterval">"""
+	print """<form action="/cgi-bin/webgui.py" method="POST">
+	Show the temperature logs for
+	<select name="timeinterval">"""
 
-
-    if option is not None:
-
-        if option == "6":
-            print "<option value=\"6\" selected=\"selected\">the last 6 hours</option>"
-        else:
-            print "<option value=\"6\">the last 6 hours</option>"
-
-        if option == "12":
-            print "<option value=\"12\" selected=\"selected\">the last 12 hours</option>"
-        else:
-            print "<option value=\"12\">the last 12 hours</option>"
-
-        if option == "24":
-            print "<option value=\"24\" selected=\"selected\">the last 24 hours</option>"
-        else:
-            print "<option value=\"24\">the last 24 hours</option>"
-
-    else:
-        print """<option value="6">the last 6 hours</option>
-            <option value="12">the last 12 hours</option>
-            <option value="24" selected="selected">the last 24 hours</option>"""
-
-    print """        </select>
-        <input type="submit" value="Display">
-    </form>"""
+	if option is not None:
+		if option == "6":
+			print "<option value=\"6\" selected=\"selected\">the last 6 hours</option>"
+		else:
+			print "<option value=\"6\">the last 6 hours</option>"
+		if option == "12":
+			print "<option value=\"12\" selected=\"selected\">the last 12 hours</option>"
+		else:
+			print "<option value=\"12\">the last 12 hours</option>"
+		if option == "24":
+			print "<option value=\"24\" selected=\"selected\">the last 24 hours</option>"
+		else:
+			print "<option value=\"24\">the last 24 hours</option>"
+	else:
+		print """<option value="6">the last 6 hours</option>
+		<option value="12">the last 12 hours</option>
+		<option value="24" selected="selected">the last 24 hours</option>"""
+	print """</select>
+	<input type="submit" value="Display">
+	</form>"""
 
 
 # check that the option is valid
 # and not an SQL injection
 def validate_input(option_str):
-    # check that the option string represents a number
-    if option_str.isalnum():
-        # check that the option is within a specific range
-        if int(option_str) > 0 and int(option_str) <= 24:
-            return option_str
-        else:
-            return None
-    else: 
-        return None
+	# check that the option string represents a number
+	if option_str.isalnum():
+		# check that the option is within a specific range
+		if int(option_str) > 0 and int(option_str) <= 24:
+			return option_str
+		else:
+			return None
+	else:
+		return None
+
 
 
 #return the option passed to the script
 def get_option():
-    form=cgi.FieldStorage()
-    if "timeinterval" in form:
-        option = form["timeinterval"].value
-        return validate_input (option)
-    else:
-        return None
+	form=cgi.FieldStorage()
+	if "timeinterval" in form:
+		option = form["timeinterval"].value
+		return validate_input (option)
+	else:
+		return None
 
 
 # main function
-# This is where the program starts 
+# This is where the program starts
 def main():
+	
+	# print the HTTP header
+	printHTTPheader()
+	
+	# get options that may have been passed to this script
+	option=get_option()
 
-    cgitb.enable()
+	if option is None:
+		option = str(24)
 
-    # get options that may have been passed to this script
-    option=get_option()
+	# get data from the database
+	records=get_data(option)
 
-    if option is None:
-        option = str(24)
+	if len(records) != 0:
+		# convert the data into a table
+		table=create_table(records)
+	else:
+		print "No data found"
+		return
 
-    # get data from the database
-    records=get_data(option)
+	# print the head section including the table
+	# used by the javascript for the chart
+	print "<HTML>"
+	printHTMLhead(str("Raspberry Pi Temperature Logger"),table)
+	
+	# print the page body
+	print "<body>"
+	print "<h1>Raspberry Pi Temperature Logger</h1>"
+	print "<hr>"
+	print_time_selector(option)
+	show_graph()
+	show_stats(option)
+	print "</body>"
+	print "</html>"
 
-    # print the HTTP header
-    printHTTPheader()
+	sys.stdout.flush()
 
-    if len(records) != 0:
-        # convert the data into a table
-        table=create_table(records)
-    else:
-        print "No data found"
-        return
-
-    # start printing the page
-    print "<html>"
-    # print the head section including the table
-    # used by the javascript for the chart
-    printHTMLHead("Raspberry Pi Temperature Logger", table)
-
-    # print the page body
-    print "<body>"
-    print "<h1>Raspberry Pi Temperature Logger</h1>"
-    print "<hr>"
-    print_time_selector(option)
-    show_graph()
-    show_stats(option)
-    print "</body>"
-    print "</html>"
-
-    sys.stdout.flush()
-
-if __name__=="__main__":
-    main()
+# Run the code:
+cgitb.enable()
+main()
 
